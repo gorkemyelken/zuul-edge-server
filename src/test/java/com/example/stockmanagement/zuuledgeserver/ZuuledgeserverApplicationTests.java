caeafe855a3017a2117135549fde4b18dcc0e0d2
Görkem <gorkem_yelken@hotmail.com>
2023-03-03 18:18:23 +0300

    Initial commit

---
+++ src/test/java/com/example/stockmanagement/zuuledgeserver/ZuuledgeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagement.zuuledgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuuledgeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
